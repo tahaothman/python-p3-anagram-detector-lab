680a69ebfa6580e35a67fcfb80cd1073c3be20df
Taha <taha.othman@outlook.com>
2023-07-26 16:19:40 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+import collections
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+
+    def match(self, word_list):
+        return [w for w in word_list if self.is_anagram(w.lower())]
+
+    def is_anagram(self, other_word):
+        return sorted(self.word) == sorted(other_word)
